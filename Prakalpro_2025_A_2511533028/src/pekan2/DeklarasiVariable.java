922bbb2da2668e3c362e329d3386580eb4e9fa95
ARDRA ATHAILLA FIRSY <ARDRA ATHAILLA FIRSY@LAPTOP-BUD85NC8>
2025-09-15 14:16:56 +0700

    PRAKTIKUM2 KELAS A

--- Prakalpro_2025_A_2511533028/src/pekan2/DeklarasiVariable.java
+++ Prakalpro_2025_A_2511533028/src/pekan2/DeklarasiVariable.java
@@ -1,5 +1,22 @@
 package pekan2;
 
 public class DeklarasiVariable {
-
+     /* program java
+      * latihan
+      * tentang pendeklarasian variable
+      */
+	static int umur=25;  /* variable dapat langsung diinisasi */
+	public static void main(String[] args) {
+		int kode;
+		boolean isDibawahUmur; /* perhatikan penulisan nama variable */
+		kode = 1234; /* pengisian variable (assigment)*/
+		double gaji; /* deklarasi variable dapat dimana saja */
+		gaji = 5500000.23;
+		isDibawahUmur = true;
+		System.out.println("Status: "+isDibawahUmur);
+		System.out.println("Kode:"+kode);
+		System.out.println("Umur:"+umur);
+		System.out.println("Gaji:"+gaji);
+	}
+	
 }
