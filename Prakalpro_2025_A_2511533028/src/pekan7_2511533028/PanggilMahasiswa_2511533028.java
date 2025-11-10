7c361f4763591bc53c77868fa482d105ef27b970
ARDRA ATHAILLA FIRSY <ARDRA ATHAILLA FIRSY@LAPTOP-BUD85NC8>
2025-11-10 14:42:50 +0700

    pekan 6 2511533028

---
+++ Prakalpro_2025_A_2511533028/src/pekan7_2511533028/PanggilMahasiswa_2511533028.java
@@ -0,0 +1,13 @@
+package pekan7_2511533028;
+
+public class PanggilMahasiswa_2511533028 {
+	public static void main(String[] args) {
+		Mahasiswa_2511533028 a= new Mahasiswa_2511533028();
+		a.setNim(33028);
+		a.setNama("Ardra Athailla Firsy");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+
+}
---
+++ Prakalpro_2025_A_2511533028/src/pekan7_2511533028/String1_2511533028.java
@@ -0,0 +1,12 @@
+package pekan7_2511533028;
+
+public class String1_2511533028 {
+	public static void main(String[] args) {
+		String salam = "Assalamualaikum";
+		System.out.println("panjang salam adalah: " + salam.length());
+		System.out.println(salam.toUpperCase());   // Outputs "ASSALAMUALAIKUM"
+		System.out.println(salam.toLowerCase());   // Outputs "assalamualaikum"
+		System.out.println(salam.indexOf("salam")); // Outputs 2
+	}
+
+}
